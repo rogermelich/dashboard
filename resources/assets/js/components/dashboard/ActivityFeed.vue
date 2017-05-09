7f4fb838062c7f1a2713dab079f7a3f6e51d8d86
Roger Melich <rogermelich@gmail.com>
2017-05-09 17:34:16 +0200

    Add Title

--- resources/assets/js/components/dashboard/ActivityFeed.vue
+++ resources/assets/js/components/dashboard/ActivityFeed.vue
@@ -12,7 +12,7 @@
             <div class="timeline-item">
                 <span class="time"><i class="fa fa-clock-o"></i> {{ activity.updated_at }}</span>
 
-                <h3 class="timeline-header">{{ title }}</h3>
+                <h3 class="timeline-header">{{ getTitle(activity.type) }}</h3>
 
                 <div class="timeline-body">
                     TODO DESCRIPTION {{ activity.type }}
@@ -34,14 +34,21 @@
                 activities : []
             }
         },
-        computed: {
-            title
-        },
         mounted() {
             console.log('Component activity feed!')
             this.fetchActivityFeed()
         },
         methods: {
+            getTitle: function (type) {
+                switch(type) {
+                    case 'created_task':
+                        return 'Tasca creada'
+                    case 'created_thread':
+                        return 'Thread creat'
+                    default:
+                        return 'TODO title'
+                }
+            },
             fetchActivityFeed () {
                 console.log('fetchActivityFeedg executed!')
                 var component = this
